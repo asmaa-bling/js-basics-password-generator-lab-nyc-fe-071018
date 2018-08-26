1d8de8c046ddb1472314505211e0c687eb809978
Asmaa Abbas <asmaa@flatironschool.com>
2018-08-26 22:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", () => {
   //start your code here
+  document.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.getElementById('passwordLength');
+    let passwordLength = input.value;
+    let lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz'.split();
+  });
 });
